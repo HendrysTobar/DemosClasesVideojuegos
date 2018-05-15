b51752d16cd88e14799e68538436419b4f2c32f6
Hendrys Tobar <fabiantobar@gmail.com>
2018-05-15 01:00:48 -0500

    Demo Animaciones

---
+++ Demo Animaciones/Assets/Cannon.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour 
+{
+    #region Enums
+
+
+    #endregion
+
+    #region Atributos y Propiedades
+    public GameObject bala;
+    public Transform punta;
+	
+    #endregion
+	
+    #region Eventos    
+	
+	
+    #endregion
+	
+    #region Mensajes Unity
+	
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    #endregion
+
+    public void Disparar()
+    {
+        Animator anim = GetComponent<Animator>();
+        
+        anim.Play("Disparar");
+    }
+
+    #region Métodos
+    public void Boom()
+    {
+        Instantiate(bala, punta.position,bala.transform.rotation);
+    }
+	
+    #endregion
+    #region CoRutinas
+	
+	
+	#endregion
+}
---
+++ Demo Animaciones/Assets/Fruto.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruto : MonoBehaviour 
+{
+	#region Enums
+	
+	
+    #endregion
+	
+    #region Atributos y Propiedades
+	
+	
+    #endregion
+	
+    #region Eventos    
+	
+	
+    #endregion
+	
+    #region Mensajes Unity
+	
+	void Start ()
+    {
+        Rigidbody rgb = GetComponent<Rigidbody>();
+        rgb.AddForce(100 * Vector3.one);
+        Destroy(this.gameObject, 5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	#endregion
+	
+    #region Métodos
+	
+	
+    #endregion
+    #region CoRutinas
+	
+	
+	#endregion
+}
